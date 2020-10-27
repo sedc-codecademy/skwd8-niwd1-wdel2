b3b449c45065a0904afee290bb67f78f48f6fdc8
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-10-27 20:02:01 +0100

    Handling multiple methods on a single route.

--- node-lesson-three/index.js
+++ node-lesson-three/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/flights/:from-:to', function(req, res) {
-    const params = req.params;
-    console.log('Get is called for /about!', params);
-    res.status(200);
-    res.send('Hello there!');
-});
+
+app.route('/users')
+    .get(function(req, res) {
+    res.send('Hello GET!');
+    })
+    .post(function(req, res) {
+        res.send('Hello POST!');
+    });
 
 app.listen(port, function() {
     console.log(`Server listening on port ${port}...`);
