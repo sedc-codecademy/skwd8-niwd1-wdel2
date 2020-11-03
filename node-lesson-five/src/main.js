3055da09598557364a274d5caba49f78455611d7
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-11-03 18:24:23 +0100

    Create Express app

--- node-lesson-five/src/main.js
+++ node-lesson-five/src/main.js
@@ -1,8 +1,19 @@
 const database = require('./modules/database');
+const config = require('./config');
 
 (async () => {
     console.info('Starting the app...');
     console.info('Connecting to the DB...');
     await database.connect();
     console.info('Connected to the DB.');
+
+    console.info('Creating the express app...')
+    const express = require('express');
+    const app = express();
+
+    console.info('Starting the app...');
+    app.listen(config.app.port, () => {
+        console.info(`Server listening on port ${config.app.port}...`)
+    });
+
 })();
