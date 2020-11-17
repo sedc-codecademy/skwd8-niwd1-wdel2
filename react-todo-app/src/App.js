e31564b84311d996f41a7c082396e2b27483544a
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-11-17 20:05:40 +0100

    Basic routing

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,5 +1,28 @@
+import { useSelector } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 export default function App() {
+
+  const loggedIn = useSelector(state => state.auth.loggedIn);
+
   return (
-    <div>Hello!</div>
+    <Switch>
+      <Route path="/login">
+        Login
+      </Route>
+      <Route path="/register">
+        Register
+      </Route>
+      <Route path="/todos">
+        {
+          loggedIn
+          ? 'Todos!'
+          : <Redirect to="/login"/>
+        }
+      </Route>
+      <Route path="/">
+        <Redirect to="/todos"/>
+      </Route>
+    </Switch>
   );
 }
