8bf6715e2fbfb12d2fd9af9cae307c25bb5f6416
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-11-17 20:29:57 +0100

    Added CORS support.

--- node-lesson-five/src/router/index.js
+++ node-lesson-five/src/router/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { Router } = express;
 const auth = require('./auth');
 const todos = require('./todos');
 
 const mainRouter = Router();
 
+mainRouter.use(cors());
 mainRouter.use(express.json());
 mainRouter.use(auth.path, auth.router);
 mainRouter.use(todos.path, todos.router);
