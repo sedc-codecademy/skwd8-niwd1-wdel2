8085fd263da5405e61455e670ae7295c687fded7
Aleksandar Bencun <aleksandar.bencun@seavus.com>
2020-11-17 19:55:46 +0100

    Auth reducer created

---
+++ react-todo-app/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    loggedIn: false,
+};
+
+export default function authReducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
